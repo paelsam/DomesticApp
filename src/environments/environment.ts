409ed694db4142facc9230686d78610fcf2f2ec3
Elkin Angulo <elkinsamir643@gmail.com>
2024-06-10 00:52:19 -0500

    Añadiendo link de desarrollo de la API

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  baseUrl: 'http://localhost:4200/api',
+  baseUrl: 'http://172.19.0.3:80',
   production: false
 };
 
