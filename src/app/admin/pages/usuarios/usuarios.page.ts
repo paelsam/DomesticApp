2e663244e9521b36c550943a460dab26fe7ff8d5
SalomeAc <salome.acosta@correounivalle.edu.co>
2024-06-17 00:28:20 -0500

    usuarios (de admin)

--- src/app/admin/pages/usuarios/usuarios.page.ts
+++ src/app/admin/pages/usuarios/usuarios.page.ts
@@ -8,8 +8,11 @@ import { Component, OnInit } from '@angular/core';
 export class UsuariosPage implements OnInit {
 
   constructor() { }
-
+  items: string[] = []; 
   ngOnInit() {
+    for (let i = 1; i < 51; i++) {
+      this.items.push(`${i}`);
+    }
   }
 
 }
